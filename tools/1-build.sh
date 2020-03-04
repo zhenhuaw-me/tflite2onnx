6661f448471ed9976b1517a132257d7e56f07d72
王振华 (Zhenhua WANG) <i@jackwish.net>
2020-03-04 06:25:04 -0800

    tools: build and upload tools

---
+++ tools/1-build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ "$(uname -s)" == "Darwin" ]; then
+  root_dir=$(dirname $(dirname $(greadlink -f $0})))
+else
+  root_dir=$(dirname $(dirname $(readlink -f $0})))
+fi
+rm -f ${root_dir}/assets/dist/tflite2onnx-*.whl
+
+python ${root_dir}/setup.py bdist_wheel \
+  --bdist-dir ${root_dir}/assets/build \
+  --dist-dir ${root_dir}/assets/dist
+rm -rf ${root_dir}/tflite2onnx.egg-info
+rm -rf ${root_dir}/build
---
+++ tools/2-upload.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ "$(uname -s)" == "Darwin" ]; then
+  root_dir=$(dirname $(dirname $(greadlink -f $0})))
+else
+  root_dir=$(dirname $(dirname $(readlink -f $0})))
+fi
+
+${root_dir}/tools/1-build.sh
+
+read -p "Will upload to test.pypi.org, for real publishment type \"Release\": " input_str
+if [ -z "${input_str}" -o ${input_str} != "Release" ]; then
+  python3 -m twine upload \
+    --repository-url https://test.pypi.org/legacy/ \
+    ${root_dir}/assets/dist/tflite2onnx-*
+else
+  read -p "Will publish the package, are you sure to continue [Y|N] ? " input_str
+  if [ -n "${input_str}" -a ${input_str} = "Y" ]; then
+    echo "Uploading..."
+    python3 -m twine upload ${root_dir}/assets/dist/tflite2onnx-*
+  fi
+fi
