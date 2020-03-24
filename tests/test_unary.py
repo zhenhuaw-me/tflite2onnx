dd2d4944658602a5c23cfc3f9a97d3348ccd498d
王振华 (Zhenhua WANG) <i@jackwish.net>
2020-03-24 00:46:15 -0700

    compare result of ONNX and TFLite
    with the help of latest `shrub`.

--- tests/test_unary.py
+++ tests/test_unary.py
@@ -1,4 +1,5 @@
 import logging
+import shrub
 import tflite2onnx as t2o
 import util_for_test
 
@@ -6,9 +7,17 @@ logging.basicConfig(format='[%(name)s:%(levelname)6s] [%(filename)12s:%(lineno)3
                     level=logging.DEBUG)
 
 OP_LIST = (
-        'abs.int32',
+        'abs.float32',
         )
 
 for op in OP_LIST:
     tflm_path = util_for_test.getTFLiteModel(op)
     t2o.convert(tflm_path, op + '.onnx')
+
+    m = shrub.tflite.parse(tflm_path)
+    m.genInput()
+
+    onnx_ret = shrub.onnx.run(op + '.onnx', m.inputs)
+    tflite_ret = shrub.tflite.run(tflm_path, m.inputs)
+    assert(shrub.network.cmpTensors(onnx_ret, tflite_ret))
+
