dc6c1b493e48c6d6f857794e6011b58d5446b017
王振华 (Zhenhua WANG) <i@jackwish.net>
2020-03-28 02:11:32 -0700

    GitHub: improve CI action

--- tests/test_ops.py
+++ tests/test_ops.py
@@ -10,14 +10,14 @@ OP_LIST = (
         'abs.float32',
         )
 
-for op in OP_LIST:
-    tflm_path = util_for_test.download(op + '.tflite')
-    t2o.convert(tflm_path, op + '.onnx')
+def test_ops():
+    for op in OP_LIST:
+        tflm_path = util_for_test.download(op + '.tflite')
+        t2o.convert(tflm_path, op + '.onnx')
 
-    m = shrub.tflite.parse(tflm_path)
-    m.genInput()
+        m = shrub.tflite.parse(tflm_path)
+        m.genInput()
 
-    onnx_ret = shrub.onnx.run(op + '.onnx', m.inputs)
-    tflite_ret = shrub.tflite.run(tflm_path, m.inputs)
-    assert(shrub.network.cmpTensors(onnx_ret, tflite_ret))
-
+        onnx_ret = shrub.onnx.run(op + '.onnx', m.inputs)
+        tflite_ret = shrub.tflite.run(tflm_path, m.inputs)
+        assert(shrub.network.cmpTensors(onnx_ret, tflite_ret))
