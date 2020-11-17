53d18529def4ba81216be740e6320371e4612f61
Mingrui <mrz@zju.edu.cn>
2020-11-17 23:39:34 +0000

    OP: add preserve input/output spatial semantic for `Reshape`

--- tflite2onnx/op/reshape.py
+++ tflite2onnx/op/reshape.py
@@ -43,15 +43,15 @@ class Reshape(Operator):
         # because the `Reshape` with only one input seems like a special case
         # haven't manage to reproduce currently
         # data = self.parseInput(0)
-        if op.InputsLength() == 1:
-            # options
-            op_opt = op.BuiltinOptions()
-            option = tflite.ReshapeOptions()
-            option.Init(op_opt.Bytes, op_opt.Pos)
-            sp = option.NewShapeAsNumpy()
-            sp = self.TFactory.createVector(sp.astype('int64'))
-            sp.addConsumer(self)
-            self.inputs.append(sp)
+        # if op.InputsLength() == 1:
+        #     # options
+        #     op_opt = op.BuiltinOptions()
+        #     option = tflite.ReshapeOptions()
+        #     option.Init(op_opt.Bytes, op_opt.Pos)
+        #     sp = option.NewShapeAsNumpy()
+        #     sp = self.TFactory.createVector(sp.astype('int64'))
+        #     sp.addConsumer(self)
+        #     self.inputs.append(sp)
 
         if op.InputsLength() == 2:
             # shape
@@ -71,28 +71,51 @@ class Reshape(Operator):
         # output
         self.parseOutput(0)
 
-        # Insert a `Transpose`
-        self.fakeTranspose()
-
         self.setParsed()
 
-    def fakeTranspose(self):
+    def preserveInputSpatialSemantic(self):
         # If the input (or output) tensor of Reshape
         # holds a special data layout semantic,
         # we need to insert Transpose before (or after) the Reshape operator
         # https://github.com/jackwish/tflite2onnx/issues/28
+        # An example for inserting `Transpose` before `Reshape`
+        #    ------
+        #   | Conv |
+        #    ------
+        #      |
+        #      |  to_transpose (Original input of `Reshape`)
+        #      |  (e.g. NCHW)
+        #   -------
+        #  | Trans |  e.g. perm: (0, 2, 3, 1)
+        #   -------
+        #      |
+        #      |  transposed (New created tensor)
+        #      |  (e.g. NHWC)
+        #   --------
+        #  | Reshape |
+        #   --------
+        #
 
-        assert(self.status.initialized)
+        assert(self.status.parsed)
         to_transpose = self.inputs[0]
 
         transposed_name = 'TFLITE2ONNX_Transposed_%s' % to_transpose.name
         transposed = self.TFactory.getWithRef(to_transpose, transposed_name, True)
-        transposed.shape = [to_transpose.shape[p] for p in [0, 2, 3, 1]]
+
+        # Copy the layout of original input of `Reshape`
+        # e.g. current: NCHW, (source: NHWC, target: NCHW)
+        layout = copy.deepcopy(to_transpose.layout)
+        # Exchange the source and target layout for layout preservation
+        layout.current = layout.source
+        layout.source = layout.target
+        layout.target = layout.current
+        transposed.shape = layout.transform(to_transpose.shape)
         transposed.setParsed()
 
-        # Construct the additional transpose
+        # Construct the additional transpose before `Reshape`
         trans = Transpose(self.TFactory, -1)
-        trans.attrs['perm'] = np.array([0, 2, 3, 1])
+        # e.g. From NCHW to NHWC
+        trans.attrs['perm'] = layout.perm
 
         trans.inputs.append(to_transpose)
         to_transpose.replaceConsumer(self, trans)
@@ -105,17 +128,88 @@ class Reshape(Operator):
 
         self.pre.append(trans)
 
+    def preserveOutputSpatialSemantic(self):
+        # If the input (or output) tensor of Reshape
+        # holds a special data layout semantic,
+        # we need to insert Transpose before (or after) the Reshape operator
+        # https://github.com/jackwish/tflite2onnx/issues/28
+
+        # An example for inserting `Transpose` after `Reshape`
+        #   -------
+        # | Reshape |
+        #   -------
+        #      |
+        #      |  to_transpose (New created tensor)
+        #      |  (e.g. NHWC)
+        #   -------
+        #  | Trans |  e.g. perm: (0, 3, 1, 2)
+        #   -------
+        #      |
+        #      |  transposed (Original `Reshape` output)
+        #      |  (e.g. NCHW)
+        #    ------
+        #   | Conv |
+        #    ------
+        #
+
+        assert(self.status.parsed)
+        transposed = self.outputs[0]
+
+        transposed_new_name = 'TFLITE2ONNX_Transposed_%s' % transposed.name
+        to_transpose_name = transposed.name
+        # Rename the transposed tensor
+        # because it will be the output of the new `transpose`
+        self.TFactory.registery[transposed_new_name] = \
+            self.TFactory.registery.pop(transposed.name)
+        transposed.name = transposed_new_name
+        to_transpose = self.TFactory.getWithRef(transposed, to_transpose_name, True)
+
+        # Copy the layout of original output of `Reshape`
+        # e.g. current: NHWC, (source: NCHW, target: NHWC )
+        layout = copy.deepcopy(transposed.layout)
+        # Exchange the source and target layout for layout preservation
+        layout.current = layout.source
+        layout.source = layout.target
+        layout.target = layout.current
+        to_transpose.shape = layout.transform(transposed.shape)
+        to_transpose.setParsed()
+
+        # Construct the additional transpose after `Reshape`
+        trans = Transpose(self.TFactory, -1)
+        # e.g. From NHWC to NCHW
+        trans.attrs['perm'] = transposed.layout.perm
+
+        trans.inputs.append(to_transpose)
+        transposed.replaceProducer(self, trans)
+        self.replaceOutput(transposed, to_transpose)
+
+        trans.outputs.append(transposed)
+        to_transpose.addProducer(self)
+        to_transpose.addConsumer(trans)
+        trans.setParsed()
+
+        self.post.append(trans)
+
     def propagatableTensors(self):
         return list()
 
     def transform(self):
-        if not self.forFakeBroadcasting:
-            return
         i = self.inputs[0]
         o = self.outputs[0]
-        assert(len(i.shape) != len(o.shape))
-        shape_t = self.inputs[1]
-        layout = copy.deepcopy(o.layout)
-        if layout is None:
-            raise ValueError("Requires layout description for <%s>" % i.name)
-        shape_t.data = layout.transform(shape_t.data)
+
+        if self.forFakeBroadcasting:
+            assert(len(i.shape) != len(o.shape))
+            shape_t = self.inputs[1]
+            layout = copy.deepcopy(o.layout)
+            if layout is None:
+                raise ValueError("Requires layout description for <%s>" % i.name)
+            shape_t.data = layout.transform(shape_t.data)
+        else:
+            if i.layout:
+                # Insert a `Transpose` before `Reshape`
+                # if a required layout has propagated here
+                self.preserveInputSpatialSemantic()
+            if o.layout:
+                # Insert a `Transpose` after `Reshape`
+                # if a explicit layout is required later
+                self.preserveOutputSpatialSemantic()
