8d0b014cad72d37c2efe591e107dee485c4cce45
王振华 (Zhenhua WANG) <i@jackwish.net>
2021-01-03 15:26:32 +0800

    version and url in command line (#46)

--- tflite2onnx/__init__.py
+++ tflite2onnx/__init__.py
@@ -7,7 +7,8 @@ from tflite2onnx.misc import enableDebugLog, getSupportedOperator
 NAME = 'tflite2onnx'
 VERSION = '0.3.1'
 __version__ = VERSION
-DESCRIPTION = "Convert TensorFlow Lite models to ONNX models"
+DESCRIPTION = """Convert TensorFlow Lite models to ONNX models.
+tflite2onnx v%s, https://github.com/jackwish/tflite2onnx""" % VERSION
 
 __all__ = [
     convert,
--- tflite2onnx/convert.py
+++ tflite2onnx/convert.py
@@ -47,9 +47,11 @@ def convert(tflite_path: str, onnx_path: str, explicit_layouts=None):
 
 
 def cmd_convert():
-    parser = argparse.ArgumentParser(description=tflite2onnx.DESCRIPTION)
+    parser = argparse.ArgumentParser(description=tflite2onnx.DESCRIPTION,
+                                     formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument('tflite_path', help="Path to the input TFLite mode")
     parser.add_argument('onnx_path', help="Path to save the converted ONNX mode")
 
     args = parser.parse_args()
+
     convert(args.tflite_path, args.onnx_path)
