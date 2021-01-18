51ea312dc664e33bb3ffa84809782b4100a37c2e
王振华 (Zhenhua WANG) <i@jackwish.net>
2021-01-18 23:00:58 +0800

    Apply suggestions from code review

--- tflite2onnx/__init__.py
+++ tflite2onnx/__init__.py
@@ -7,8 +7,7 @@ from tflite2onnx.misc import enableDebugLog, getSupportedOperator
 NAME = 'tflite2onnx'
 VERSION = '0.3.1'
 __version__ = VERSION
-DESCRIPTION = 'Convert TensorFlow Lite models to ONNX models. ' \
-              'tflite2onnx v%s, https://github.com/jackwish/tflite2onnx' % VERSION
+DESCRIPTION =  "tflite2onnx v%s, Convert TensorFlow Lite models to ONNX" % VERSION
 
 __all__ = [
     convert,
